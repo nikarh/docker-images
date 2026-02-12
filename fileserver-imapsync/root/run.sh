59e06c677d0f0274385847ec246aba8e6a4153c3
Nikolay Arhipov <n@arhipov.net>
2026-02-13 01:25:35 +0200

    Change goimapnotify command to use log-level option

--- fileserver-imapsync/root/run.sh
+++ fileserver-imapsync/root/run.sh
@@ -3,4 +3,4 @@
 echo "$(sed 's/${USERNAME}/'"$USERNAME"'/g' /mbsync.conf)" > /mbsync.conf
 
 mbsync -LV -c /mbsync.conf gmail
-goimapnotify -debug -conf /imapnotify.conf
+goimapnotify -log-level debug -conf /imapnotify.conf
